d234ade10e83ac4bc7c9d92f3adf4c482a52edc7
Prathyusha Davuluru <dprathyusha252@gmail.com>
2020-07-29 11:09:47 -0400

    Corrected few minor errors

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,10 +8,8 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'Prathyushas AWESOME Project';
   emittedEvent = "";
-  myFunction(): void { 
-       alert('Successfully bound!'); 
-   } 
-   emitThisEvent($event){
+
+  emitThisEvent($event){
       this.emittedEvent = $event;
-   }
+  }
 }
